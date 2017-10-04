0dea924aa2b5df85ac20fde6d23ebece3512265a
Sergio Gomes <sergiofgomes117@gmail.com>
2017-10-04 11:23:24 -0600

    blocked non admin users from accessing audit log index page

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,4 +1,4 @@
-# require 'rails_helper'
+require 'rails_helper'
 
 describe 'navigate' do
   let(:user) { FactoryGirl.create(:user) }
@@ -32,7 +32,7 @@ describe 'navigate' do
     end
 
     it 'has a scope so that only post creators can see their posts' do
-      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf", phone: "5555555555")
       post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id, overtime_request: 3.5)
 
       visit posts_path
